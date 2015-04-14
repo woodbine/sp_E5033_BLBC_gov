6e9ede110052e73a6b03645c3b73f23fa6dc0655
woodbine <ian@spendnetwork.com>
2015-04-14 13:03:39 +0100

    drag out the <ul> without any class attribute

--- scraper.py
+++ scraper.py
@@ -37,7 +37,7 @@ for block in blocks:
 	
 	csvYr = soup2.find('span',{'class':'h1Text'}).contents[0].strip()
 	
-	formBlock = soup2.findAll('ul') # find the block containing the links
+	formBlock = soup2.findAll('ul' attrs={'class': None}) # finds the UL without any class
 	print "++++++++++++++++++++++++++++++++++++++++++"
 	print formBlock
 	'''
